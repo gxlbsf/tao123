b7cc051f0c9565a1ca08fe4ff33806e5ec1e555b
gxlbsf <74660260+gxlbsf@users.noreply.github.com>
2020-11-30 22:58:31 +0800

    Create 113002.接口.R.java

---
+++ 113002.接口.R.java
@@ -0,0 +1,9 @@
+package pac;
+public interface R{
+
+    static void HuaBei() {
+            String boy_filePath = "img/boy.gif";
+            String girl_filePath = "img/girl.gif";
+
+            }
+    }
